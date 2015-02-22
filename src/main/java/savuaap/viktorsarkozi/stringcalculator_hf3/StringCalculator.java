c4ee8e39d1c412a14b78afe0488e33da86567cc0
ViktorSarkozi <s.wictor898@gmail.com>
2015-02-22 16:26:52 +0100

    Osztály létrehozva

---
+++ src/main/java/savuaap/viktorsarkozi/stringcalculator_hf3/StringCalculator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package savuaap.viktorsarkozi.stringcalculator_hf3;
+
+/**
+ *
+ * @author SAVUAAP.PTE
+ */
+public class StringCalculator {
+    
+}
