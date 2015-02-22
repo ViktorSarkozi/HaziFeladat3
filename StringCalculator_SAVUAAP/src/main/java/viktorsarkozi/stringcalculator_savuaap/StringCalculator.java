2cf574bd3530965e68f807003c3b6f67cd7d75f6
ViktorSarkozi <s.wictor898@gmail.com>
2015-02-22 19:32:54 +0100

    metódus üres stringet elfogad

--- StringCalculator_SAVUAAP/src/main/java/viktorsarkozi/stringcalculator_savuaap/StringCalculator.java
+++ StringCalculator_SAVUAAP/src/main/java/viktorsarkozi/stringcalculator_savuaap/StringCalculator.java
@@ -6,5 +6,16 @@ package viktorsarkozi.stringcalculator_savuaap;
  * @author SAVUAAP.PTE
  */
 public class StringCalculator {
-    
+ 
+    private int add(String numbers){
+        String[] numbersInArray=numbers.split(",");
+        switch (numbersInArray.length){
+            case 1: {
+                switch(numbersInArray[0]){
+                    case "": return 0;
+                }
+            }
+            default: return 100;
+        }
+    }
 }
