bd6ee6aeb09877bfb71c70cdd668f772aa31bc2b
ViktorSarkozi <s.wictor898@gmail.com>
2015-02-22 16:35:47 +0100

    metódus üres stringet elfogad

--- src/main/java/savuaap/viktorsarkozi/stringcalculator_hf3/StringCalculator.java
+++ src/main/java/savuaap/viktorsarkozi/stringcalculator_hf3/StringCalculator.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package savuaap.viktorsarkozi.stringcalculator_hf3;
 
 /**
@@ -10,5 +6,12 @@ package savuaap.viktorsarkozi.stringcalculator_hf3;
  * @author SAVUAAP.PTE
  */
 public class StringCalculator {
-    
+    private int add(String numbers){
+        switch (numbers){
+            case "": return 0;
+            case "1":
+            case "2":
+            default:
+        }
+    }
 }
